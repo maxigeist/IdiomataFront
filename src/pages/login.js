6bd5fbd77433b34cb8197a35aa5cac10cc72711f
maxigeist <geist.maximo@gmail.com>
2023-03-24 15:57:44 -0300

    Language update

--- src/pages/login.js
+++ src/pages/login.js
@@ -22,13 +22,8 @@ class Login extends Component{
                     <form className='formulario'>
                         <input type="text" placeholder = "Email" required id="email_input" />
                         <input type='password' placeholder = "Password" id='password' required/>
-<<<<<<< HEAD:src/pages/login.js
                         <button className='boton-submit' onClick={this.HandleLogin}>Log In</button>
                         <button className='boton-submit' onClick={this.HandleRegister}> Register</button>
-=======
-                        <button className='boton-submit' onClick={this.input_data}>Log In</button>
-                        <button className='boton-submit' onClick={this.HandleClick}>Sign Up</button>
->>>>>>> 596cd93ed2ea234252020846a8c679d12f162e9e:src/login.js
                     </form>
                 </div>
             </div>
