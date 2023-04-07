76aedf01323c4aea34112135a013571d4749dcfe
maxigeist <geist.maximo@gmail.com>
2023-04-07 15:39:02 -0300

    Admin

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -4,6 +4,8 @@ import Login from "../pages/login";
 import Register from "../pages/register";
 import homepage from "../pages/homepage";
 import readAndWrite from "../pages/readAndWrite";
+import Admin from "../pages/admin";
+import AdminHub from "../pages/adminhub";
 
 
 class OurRoutes extends Component{
@@ -16,6 +18,8 @@ class OurRoutes extends Component{
                     <Route exact path="/register" Component={Register}></Route>
                     <Route exact path ="homepage" Component={homepage}></Route>
                     <Route exact path ="/readAndWrite" Component={readAndWrite}></Route>
+                    <Route exact path ="/admin" Component={Admin}></Route>
+                    <Route exact path ="/adminpage" Component={AdminHub}></Route>
                 </Routes>
             </BrowserRouter>
 
---
+++ src/pages/admin.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import "../style/admin.css"
+
+
+
+class Admin extends Component{
+
+    constructor(props){
+        super(props)
+        this.handleLogIn = this.handleLogIn.bind(this);
+    }
+
+    render(){
+        return(
+            <div className="Admin-form-div">
+                <form className="Admin-form">
+                    <label className="admin-label">Username</label>
+                    <input className="admin-input"></input>
+                    <label className="admin-label">Password</label>
+                    <input className="admin-input" ></input>
+                    <button className="submit-button" onClick={this.handleLogIn} >Log In</button>
+                </form>
+            </div>
+
+        );
+    }
+
+    handleLogIn = (e) => {
+        e.preventDefault()
+        window.location.href = "/adminpage";
+        
+    }
+     
+}
+
+
+export default Admin;
