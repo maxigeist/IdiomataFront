518ee1a2fb7403807f1df02dcbfc285da38adfcf
santi <santiago.peres@ing.austral.edu.ar>
2023-04-18 17:01:19 -0300

    check word with translation in RandW

--- src/pages/readAndWrite.js
+++ src/pages/readAndWrite.js
@@ -5,6 +5,7 @@ import NavBar from "../components/navbar";
 import GamesDisplay from "../components/gamesdisplay";
 import WordRequester from "../util/requester/wordRequester";
 import LanguageSelector from "../components/languageSelector";
+import {FaCheck, FaTimes} from 'react-icons/fa';
 
 class readAndWrite extends Component{
 
@@ -13,7 +14,7 @@ class readAndWrite extends Component{
     constructor(props){
         super(props)
 
-        this.state = {language: "", category: "", difficulty: "", words:[], shownword:"", translations: [], wordInput: "", limit: undefined}
+        this.state = {language: "", category: "", difficulty: "", words:[], shownword:"", translations: [], wordInput: "", limit: undefined, answerCorrectly: null, correctAnswer: ""}
 
         this.handleLanguageChange = this.handleLanguageChange.bind(this)
         this.showWords = this.showWords.bind(this)
@@ -26,6 +27,7 @@ class readAndWrite extends Component{
     }
 
     render(){
+        console.log(this.state.wordInput)
         return(
             <div className="principal-container">
                 <NavBar/>
@@ -33,8 +35,12 @@ class readAndWrite extends Component{
                 <LanguageSelector func={this.handleLanguageChange}/>
                 <button className="raw-input" onClick={this.showWords}>Next Word</button>
                 <div className="raw-input">{this.state.shownword}</div>
-                <input className="raw-input" onChange={this.handleWordInput}></input>
+                <input className="raw-input" onChange={this.handleWordInput} value={this.state.wordInput}></input>
                 <button className="raw-input" onClick={this.handleCheck}>Check</button>
+                {this.state.answerCorrectly === true && <FaCheck className="raw-input" style={{color: 'green'}}/>}
+                {this.state.answerCorrectly === false && <FaTimes className="raw-input" style={{color: 'red'}}/>}
+                {this.state.answerCorrectly === true && <p className="raw-input">Well done!</p>}
+                {this.state.answerCorrectly === false && <p className="raw-input">You answered incorrectly, the correct answer is "{this.state.correctAnswer}".</p>}
             </div>
         );
     }
@@ -52,6 +58,9 @@ class readAndWrite extends Component{
         const word = this.state.words[Math.floor(Math.random()*this.state.words.length)]
         this.setState({shownword: word.inEnglish})
         this.setState({translations: word.translations})
+        this.setState({answerCorrectly: null})
+        this.setState({correctAnswer: ""})
+        this.setState({wordInput: ""})
     }
 
     handleWordInput(event){
@@ -62,8 +71,12 @@ class readAndWrite extends Component{
         for (const translation in this.state.translations) {
             if (Object.hasOwnProperty.call(this.state.translations, translation)) {
                 const element = this.state.translations[translation];
+                this.setState({correctAnswer: element})
                 if(element === this.state.wordInput){
-
+                    this.setState({answerCorrectly: true})
+                }
+                else{
+                    this.setState({answerCorrectly: false})
                 }
             }
         }
