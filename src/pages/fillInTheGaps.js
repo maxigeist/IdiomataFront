9096923ac2b7571b96f15686bb15d35ee98b38af
Matias Bregoli <matiasbregoli@sirius.com.ar>
2023-05-20 19:55:54 -0300

    Added next and check to sentences

--- src/pages/fillInTheGaps.js
+++ src/pages/fillInTheGaps.js
@@ -12,14 +12,16 @@ class FillInTheGaps extends Component{
 
     constructor(props){
         super(props)
-        this.state= ({language:"", category:"", difficulty:"", sentence_parts:[[]],sentence_blanks:[[]], shownSentence:""})
+        this.state= ({language:"", category:"", difficulty:"", sentence_parts:[], sentence_blanks:[], shownParts: [], shownBlanks: [], inputs:[], answers: [], isCorrect:[]})
 
 
         this.handleLanguageChange = this.handleLanguageChange.bind(this)
         this.handleCategoryChange = this.handleCategoryChange.bind(this)
         this.handleDifficultyChange = this.handleDifficultyChange.bind(this)
+        this.nextSentence =  this.nextSentence.bind(this)
         this.showSentence =  this.showSentence.bind(this)
         this.loadSentences = this.loadSentences.bind(this)
+        this.handleCheck = this.handleCheck.bind(this)
 
     }
 
@@ -52,17 +54,12 @@ class FillInTheGaps extends Component{
                                 </div>
                                 
                             </div>
-                        <form>
-                        <div className="form-label"></div>
-                           
-                        
+
+                        <form onSubmit={this.handleCheck}>
+                            <this.showSentence shownParts={this.state.shownParts} shownBlanks={this.state.shownBlanks} isCorrect={this.state.isCorrect}/>
                         </form>
-                        
-                        <div>
-                        <this.showSentence/>
-                        </div>
-                        <button className="btn btn-success m-2" >Check</button>
-                        <button className="btn btn-primary" >Next Sentence</button>
+                        <button className="btn btn-success m-2" onClick={this.handleCheck}>Check</button>
+                        <button className="btn btn-primary" onClick={this.nextSentence}>Next Sentence</button>
                     </div>
                 
                 </div>
@@ -75,7 +72,7 @@ class FillInTheGaps extends Component{
 
     async loadSentences(){
         const sentences = await this.sentenceRequester.searchSentence(this.state.language, this.state.category, this.state.difficulty, this.state.limit)
-        if(sentences.length>0){
+        if(sentences.length > 0){
             const final_sentences = []
             const final_blanks = []
             for(var j = 0; j< sentences.length;j++){
@@ -84,8 +81,8 @@ class FillInTheGaps extends Component{
                 for(var i=0; i<sentences[j].parts.length; i++){
                     sentence[i] = sentences[j].parts[i].content
                 }
-                for(var k = 0; k < sentences[j].blanks.length;k++){
-                    blanks[k] = sentences[j].blanks[k].word.inEnglish
+                for(var k = 0; k < sentences[j].blanks.length; k++){
+                    blanks[k] = sentences[j].blanks[k]
                 }
                 
                 final_sentences.push(sentence)
@@ -94,7 +91,7 @@ class FillInTheGaps extends Component{
             }
             
         
-        this.setState({sentence_parts: final_sentences, sentence_blanks: final_blanks}, async () => {if(this.state.sentence_parts.length !== 0) this.showSentence()})
+        this.setState({sentence_parts: final_sentences, sentence_blanks: final_blanks}, async () => {if(this.state.sentence_parts.length !== 0) this.nextSentence()})
         
         }
     }
@@ -114,31 +111,71 @@ class FillInTheGaps extends Component{
         this.setState({difficulty: event.target.value}, async()=> {await this.loadSentences()});
     }
     
-    showSentence(){
+    nextSentence(){
+
         if(this.state.sentence_parts.length !==  0){
+
             var index = Math.floor(Math.random()*this.state.sentence_parts.length)
-            const sentence = this.state.sentence_parts[index]
+            const shownParts = this.state.sentence_parts[index]
+            const shownBlanks = this.state.sentence_blanks[index]
 
-              
-            const show_sentence = () => {
-                const elements = [];
-                for (var i = 0; i < sentence.length; i+=2) {
-                  elements.push(
+            this.setState({shownParts: shownParts, shownBlanks: shownBlanks})
+
+        }
+
+    }
+
+    showSentence(props){
+        const elements = [];
+        for (let i = 0; i < props.shownParts.length; i+=1) {
+            if(i === props.shownParts.length - 1){
+                elements.push(
                     <div style={{display:"flex"}}>
-                      <p>{sentence[i]}</p>
-                      <input style={{border:"none", borderBottom:"2px solid", height:"25px", marginLeft:"5px"}}/>
+                        <p>{props.shownParts[i]}</p>
                     </div>
-                  );
+                );
+            }else{
+                elements.push(
+                <div style={{display:"flex"}}>
+                    <p>{props.shownParts[i]}</p>
+                    <input onChange={(event) => {this.handleAnswerChange(event, i)}} className={"form-control shadow-none " + props.isCorrect[i]}/>
+                </div>
+            );
+            }
+            
+        }
+
+        return <div>{elements}</div>;
+    }
+
+    handleAnswerChange(event, index){
+        const newAnswers = this.state.answers
+        newAnswers[index] = event.target.value
+        this.setState({answers: newAnswers})
+    }
+
+    handleCheck(event){
+        event.preventDefault();
+        let answerIndex = 0;
+        console.log(this.state.shownBlanks)
+        this.state.answers.forEach((answer, index) => {
+            if(answer){
+                const translations = this.state.shownBlanks[answerIndex].slice(1)
+                for(let i = 0; i < translations.length; i++){
+                    if(translations[i] === answer){
+                        const newIsCorrect = this.state.isCorrect
+                        newIsCorrect[index] = "is-valid"
+                        this.setState({isCorrect: newIsCorrect})
+                        answerIndex ++;
+                        return
+                    }
                 }
-                return <div>{elements}</div>;
-              };
-          
-              return show_sentence();
-            }   
-
-
-
-
+                const newIsCorrect = this.state.isCorrect
+                newIsCorrect[index] = "is-invalid"
+                this.setState({isCorrect: newIsCorrect})
+                answerIndex ++;
+            }
+        })
     }
 }
 
